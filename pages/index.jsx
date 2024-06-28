2bc9182a84d6809bbca94ecef97ee451b7bb1ddf
gurdiny <gerardour619@gmail.com>
2024-06-28 11:00:35 -0600

    Features Dising

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,17 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-black text-lg">Home page</h1>
-      <p>This is home page</p>
-      <div className="flex flex-row">
-        <h2>Vendemos de todo</h2>
+      <div className="w-full flex flex-row justify-center items-center">
+        <div className="flex w-[50%] justify-center">
+          <h2 className="font-bold text-center text-[60px]">
+            Vendemos de todo
+          </h2>
+        </div>
 
-        <div>
+        <div className=" w-[50%] flex flex-col justify-center items-center">
           <img
+            width="400px"
             src="https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/thumbnail.png"
             alt=""
           />
-          <p>Hola soy un texto</p>
         </div>
       </div>
     </Layout>
